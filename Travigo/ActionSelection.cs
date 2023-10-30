12cabacde5c30a2ca8c66fa4aec93449b5fa49d4
Vadzim Mikanovich <114032121+Vadosmik@users.noreply.github.com>
2023-10-30 13:33:34 +0000

    onlyLoginVersion

---
+++ Travigo/ActionSelection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class ActionSelection
+{
+    public List<string> Action = new List<string>() { };
+    public List<string> SelectionArrow = new List<string>() { ">" };
+
+    public int GetActionSelection
+    {
+        get
+        {
+            int ArrowPosition = 0;
+            for (int i = 0; i < Action.Count - 1; i++)
+            {
+                SelectionArrow.Add(" ");
+            }
+
+            do
+            {
+                Console.WriteLine("===TRAVIGO===\n");
+                for (int i = 0; i < Action.Count; i++)
+                {
+                    Console.WriteLine(SelectionArrow[i] + Action[i]);
+                }
+                Console.WriteLine("\n=============");
+
+                var PressedButton = Console.ReadKey().Key;
+                if (PressedButton == ConsoleKey.DownArrow & ArrowPosition < SelectionArrow.Count - 1)
+                {
+                    SelectionArrow[ArrowPosition] = " ";
+                    SelectionArrow[ArrowPosition + 1] = ">";
+                    ArrowPosition++;
+                }
+                else if (PressedButton == ConsoleKey.UpArrow & ArrowPosition > 0)
+                {
+                    SelectionArrow[ArrowPosition] = " ";
+                    SelectionArrow[ArrowPosition - 1] = ">";
+                    ArrowPosition--;
+                }
+                else if (PressedButton == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    return ArrowPosition;
+                }
+
+                Console.Clear();
+            } while (true);
+        }
+    }
+}
